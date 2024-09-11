15866873c7a8a3c09f1249277e9b18402fac0b99
divyansh <divyansh.patel@brainvire.com>
2024-09-11 17:06:43 +0530

    solved server idle issue

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const express= require('express');
-const cors = require('cors');
-require('dotenv').config();
-const groupRoute = require('./routes/group');
-const userRoute = require('./routes/user');
-const cookiesParser = require('cookie-parser')
-const connectDb = require('./config/dbConfig');
-const {app,server} = require('./socket/index')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const groupRoute = require("./routes/group");
+const userRoute = require("./routes/user");
+const cookiesParser = require("cookie-parser");
+const connectDb = require("./config/dbConfig");
+const { app, server } = require("./socket/index");
 
 app.use(express.json());
-app.use(cookiesParser())
-app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true
-}))
+app.use(cookiesParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api", groupRoute);
 app.use("/api", userRoute);
 
-
-
 const PORT = process.env.PORT || 8080;
-connectDb().then(()=>{
-    server.listen(PORT, ()=>{
-        console.log("Server running at "+ PORT);
-    })
-})
+connectDb().then(() => {
+  server.listen(PORT, () => {
+    console.log("Server running at " + PORT);
+  });
+});
+
+// keep server running
+const apiUrl1 = 'https://chatter-box-backend-zudn.onrender.com';
+const apiUrl2 = 'https://blogging-app-qwgp.onrender.com';
+setInterval(() => {
+  fetch(apiUrl1)
+    .then((data) => console.log("Page Refreshed:"))
+    .catch((error) => console.error("Error:"));
+
+  fetch(apiUrl2)
+    .then((data) => console.log("Page Refreshed:"))
+    .catch((error) => console.error("Error:", error));
+}, 20000);
